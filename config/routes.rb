8e94db4440d57eb66900d13882a2474b6b90efc3
NadimRai <rai_nadim@yahoo.com>
2017-01-07 21:00:12 -0800

    Added Username Firstname Lastname field to Registration New and Edit Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
+  
   devise_for :users, :controllers => { registrations: 'registrations' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles do
+    resources :comments
+  end
+  resources :contacts 
   root to: 'pages#index'
-  get 'hello', to: 'pages#contact'
+  get 'pages/contact'
   get 'pages/about'
+
 end
